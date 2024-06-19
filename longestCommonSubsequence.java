806ea81478f94144e9f1077208a0468e6c37a881
lujiayue <lujiayue@xiaohongshu.com>
2024-06-19 10:18:51 +0800

    最长回文子序列

--- longestCommonSubsequence.java
+++ longestCommonSubsequence.java
@@ -17,10 +17,31 @@ public class longestCommonSubsequence {
         return dp[n][m];
     }
 
+    public int longestPalindromeSubseq(String s) {
+        int n = s.length();
+        int[][] dp = new int[n + 1][n + 1];
+        String ss = new StringBuilder(s).reverse().toString();
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (s.charAt(i - 1) == ss.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i][j - 1], dp[i - 1][j]);
+                }
+            }
+        }
+        return dp[n][n];
+    }
+
+
+
     public static void main(String[] args) {
         longestCommonSubsequence obj = new longestCommonSubsequence();
         String text1 = "abcde";
         String text2 = "ace";
         System.out.println(obj.longestCommonSubsequence(text1, text2));
+        System.out.println(obj.longestPalindromeSubseq("bbbab"));
     }
+
+
 }
